59a99bcfad317cd30050a811c0a4fe41dd228f29
AdrielAmori <adriel680@gmail.com>
2023-10-23 14:52:36 -0300

    termino do desafio

---
+++ index.js
@@ -0,0 +1,15 @@
+// // Para converter os valores inseridos na entrada, utilizamos a função JavaScript parseInt() que converte a parte inicial da string em um número inteiro.
+// const posicaoInicial = parseInt(gets());
+// const totalPassos = parseInt(gets());
+
+// //TODO: Calcule a posição final do herói:
+// const posicaoFinal = ;
+
+// // Imprime a posição final
+// print("Posicao final do heroi:", posicaoFinal);
+
+
+const posicaoInicial = parseInt(gets());
+const totalPassos = parseInt(gets());
+const posicaoFinal = posicaoInicial + totalPassos;
+print("Posicao final do heroi:", posicaoFinal);
